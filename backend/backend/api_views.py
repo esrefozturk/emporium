9f643098a0f0d41118fb0195bb49883e4cdfd6c5
Esref Ozturk <esrefozturk93@gmail.com>
2023-02-19 12:38:42 +0100

    cache added

--- backend/backend/api_views.py
+++ backend/backend/api_views.py
@@ -1,3 +1,5 @@
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from rest_framework.viewsets import ModelViewSet
 
 from backend.models import Project
@@ -7,3 +9,7 @@ from backend.serializers import ProjectSerializer
 class ProjectModelViewSet(ModelViewSet):
     serializer_class = ProjectSerializer
     queryset = Project.objects.all()
+
+    @method_decorator(cache_page(60 * 5))
+    def dispatch(self, request, *args, **kwargs):
+        return super(ProjectModelViewSet, self).dispatch(request, *args, **kwargs)
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -122,3 +122,10 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
